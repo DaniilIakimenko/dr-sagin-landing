09b0fc95342c334252d7d69bc9e55917d3a9e5b2
Daniil Iakimenko <da.yakimenko@yandex.ru>
2025-12-06 18:03:28 +0300

    feat: add reviews section

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ document.addEventListener("DOMContentLoaded", function () {
     divider.appendChild(circle);
   });
 });
+
+// Swiper
+const swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
